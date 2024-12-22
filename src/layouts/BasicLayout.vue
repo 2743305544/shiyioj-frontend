98761e47048aeb5cdaf93d817d28e96859994c50
SHIYI <3401187804@qq.com>
2024-12-22 22:04:41 +0800

    项目初始化2

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,5 +1,37 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <div id="BasicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">导航栏</a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/2743305544/shiyioj-frontend" target="_blank"
+          >ShiYiOJ by shiyi</a
+        >
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+#BasicLayout {
+}
+#BasicLayout .header {
+  background: red;
+}
+#BasicLayout .content {
+  background: linear-gradient(to right, #bbb, #fff);
+}
+#BasicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
